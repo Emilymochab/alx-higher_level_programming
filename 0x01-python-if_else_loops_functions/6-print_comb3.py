934f8f127218d81547d3e3a193237a79205daec0
Emilymochab <lesegomochabang0@gmail.com>
2022-08-23 07:08:28 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-
-for n1 in range(0, 10):
-    for n2 in range(1, 10):
-        if n2 > n1:
-            print("{}{}".format(n1, n2), end='')
-            if int(str(n1) + str(n2)) != 89:
-                print(end=', ')
-    print()
+strl = "Holberton"
+str2 = "School"
+strl = strl + '' + str2
+print("Welcome to {}!".format(strl))
