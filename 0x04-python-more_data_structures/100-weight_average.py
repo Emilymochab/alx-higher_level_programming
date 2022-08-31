683a7126f92a19df3940e400b0a21a41c73a3e4f
Emilymochab <lesegomochabang0@gmail.com>
2022-08-31 05:29:51 -0700

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/bin/urs/python3
+def weight_avarage(my_list=[]):
+    if not my_list:
+        return 0
+
+    num = 0
+    den = 0
+
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        den += tup[1] 
+
+    return (num / den)
