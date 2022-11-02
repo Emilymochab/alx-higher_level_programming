f90cf95aff8ee4a05b54d6f376fc4dbfcc06ca06
Emilymochab <lesegomochabang0@gmail.com>
2022-11-02 11:58:21 -0700

    6-states.sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- create the database htbn_0d_usa and the table states 
+CREATE THE TABLE IF NOT EXISTS htbn_0d_usa;
+CTREATE THE TABLE IF NOT EXISTS htbn_0d_usa.states (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256));
